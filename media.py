f3083f54b43f6917ede0a0d1dd07aad42d1e0792
Pedro PIres <pires8x12@gmail.com>
2026-03-24 22:53:40 -0300

    Update media.py

--- media.py
+++ media.py
@@ -3,6 +3,6 @@ num_de_notas = int(input('quantas notas vc tem? '))
 nota = 0
 for i in range (num_de_notas):
     nota = float(input('qual a sua nota ? '))
-    media = (nota * nota) / num_de_notas
+    media = (nota + nota) / num_de_notas
 
 print(f'sua média de notas é {media}')
