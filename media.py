6bd9b1476cbca95bd2e9c6ead0c78858969995c6
Pedro PIres <pires8x12@gmail.com>
2026-03-24 23:00:50 -0300

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,10 @@
-print('ola gostaria de saber qual sua media de notas ')
+print('Olá! Gostaria de saber qual a sua média de notas?')
 num_de_notas = int(input('quantas notas vc tem? '))
-nota = 0
-for i in range (num_de_notas):
-    nota = float(input('qual a sua nota ? '))
-    media = (nota + nota) / num_de_notas
+total = 0.0
+nota = 0.0
+for i in range(num_de_notas):
+    nota = float(input('qual a sua nota? '))
+    total += nota
+media = total / num_de_notas
 
 print(f'sua média de notas é {media}')
